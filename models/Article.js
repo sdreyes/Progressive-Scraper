ac1e38fa33ab6ec386b38253893b9ae21a623bda
sdreyes <sheshechan90@gmail.com>
2019-03-21 17:37:31 -0400

    Set Article schema

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    artist: {
+        type: String,
+        required: true
+    },
+    album: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
