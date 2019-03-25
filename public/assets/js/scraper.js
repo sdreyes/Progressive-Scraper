ec847e5422520100b1cc02bc28c5b631b2e2e90c
sdreyes <sheshechan90@gmail.com>
2019-03-25 16:01:26 -0400

    Add click event to 'add comment' button

--- public/assets/js/scraper.js
+++ public/assets/js/scraper.js
@@ -14,10 +14,34 @@ $(function () {
                 $(".modal-body").text("Could not scrape at this time");
                 $("#articles-scraped-modal").modal("show");
             }
-        })
-    })
+        });
+    });
 
     $("#articles-scraped-modal").on("hidden.bs.modal", function() {
         location.reload();
-    })
-})
+    });
+
+    $(".save-article").on("click", function(event) {
+        event.preventDefault();
+        let articleId = $(this).attr("data-id")
+        $.ajax({
+            url: "article/" + articleId,
+            type: "PUT",
+            data: {saved: true}
+        }).then(function() {
+            location.reload();
+        });
+    });
+
+    $(".unsave-article").on("click", function(event) {
+        event.preventDefault();
+        let articleId = $(this).attr("data-id")
+        $.ajax({
+            url: "article/" + articleId,
+            type: "PUT",
+            data: {saved: false}
+        }).then(function() {
+            location.reload();
+        });
+    });
+});
