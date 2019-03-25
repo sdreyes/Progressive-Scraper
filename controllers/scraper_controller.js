69e8e20d75bed1562a9eab817033a30498879880
sdreyes <sheshechan90@gmail.com>
2019-03-25 16:44:03 -0400

    Create GET route for populating comments on a review

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -102,6 +102,21 @@ router.get("/scrape", function(req, res) {
         });
 });
 
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("comment")
+        .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+    })
+        .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+    });
+});
+
 router.put("/article/:id", function(req, res) {
     db.Article.findOneAndUpdate({ _id: req.params.id }, {
         $set: { saved: req.body.saved }
