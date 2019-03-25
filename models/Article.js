b8ecfcc5350635ba7a07a66d1f2ef0b6dae2d797
sdreyes <sheshechan90@gmail.com>
2019-03-25 12:57:56 -0400

    Add author to article model

--- models/Article.js
+++ models/Article.js
@@ -19,6 +19,10 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    author: {
+        type: String,
+        required: true
+    },
     image: {
         type: String,
         required: true
