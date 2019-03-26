3f0a3e36960eec5e8cff81cb43a1c49e81544ae6
sdreyes <sheshechan90@gmail.com>
2019-03-26 13:39:36 -0400

    Add click event for comment delete buttons, and reload the page any time a modal is closed

--- public/assets/js/scraper.js
+++ public/assets/js/scraper.js
@@ -21,6 +21,10 @@ $(function () {
         location.reload();
     });
 
+    $(".comments-modal").on("hidden.bs.modal", function() {
+        location.reload();
+    });
+
     $(".save-article").on("click", function(event) {
         event.preventDefault();
         let articleId = $(this).attr("data-id")
@@ -48,16 +52,33 @@ $(function () {
     $(".add-comment").on("click", function(event) {
         event.preventDefault();
         let articleId = $(this).attr("data-id");
+        $("#comments-modal-" + articleId).modal("show");
+    })
+
+    $(".submit-comment").on("click", function(event) {
+        event.preventDefault();
+        let articleId = $(this).attr("data-id");
         $.ajax({
-            url: "/articles/" + articleId,
-            type: "GET"
-        }).then(function(response) {
-            if (response.comments) {
-                for (i=0; i < response.comments.length; i++) {
-                    $("#comments-modal-" + articleId).html("<p>" + response.comments[i].name + "</p><p>" + response.comments[i].body + "/p>");
-                }
+            url: "/article/" + articleId,
+            type: "POST",
+            data: {
+                name: $("#name-input-" + articleId).val(),
+                body: $("#comment-area-" + articleId).val()
             }
-            $("#comments-modal-" + articleId).modal("show");
+        }).then(function() {
+            $("#comments-modal-" + articleId).modal("hide");
+        })
+    })
+
+    $(".delete-comment").on("click", function(event) {
+        event.preventDefault();
+        let commentId = $(this).attr("data-id");
+        let articleId = $(this).parent().attr("data-id");
+        $.ajax({
+            url: "/article/" + articleId + "/comment/" + commentId,
+            type: "DELETE"
+        }).then(function() {
+            $("#comments-modal-" + articleId).modal("hide");
         })
     })
 });
