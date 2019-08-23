bd52b638ea1fbffb27ef931a3d3452336d959e76
sdreyes <sheshechan90@gmail.com>
2019-08-22 20:46:03 -0400

    Add a loading spinner modal to show when the scrape is in progress and fix indentation

--- public/assets/js/scraper.js
+++ public/assets/js/scraper.js
@@ -1,95 +1,99 @@
 $(function () {
-    $(".scrape-reviews").on("click", function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: "/scrape",
-            type: "GET",
-            success: function(result) {
-                $("#articles-scraped-modal").modal("hide");
-                $(".modal-body").text(result.message);
-                $("#articles-scraped-modal").modal("show");
-            },
-            error: function() {
-                $("#articles-scraped-modal").modal("hide");
-                $(".modal-body").text("Could not scrape at this time");
-                $("#articles-scraped-modal").modal("show");
-            }
-        });
+  $(".scrape-reviews").on("click", function (event) {
+    event.preventDefault();
+    $("#scraping-articles-modal").modal("hide");
+    $("#scraping-articles-modal").modal("show");
+    $.ajax({
+      url: "/scrape",
+      type: "GET",
+      success: function (result) {
+        $("#scraping-articles-modal").modal("hide");
+        $("#articles-scraped-modal").modal("hide");
+        $(".modal-body").text(result.message);
+        $("#articles-scraped-modal").modal("show");
+      },
+      error: function () {
+        $("#scraping-articles-modal").modal("hide");
+        $("#articles-scraped-modal").modal("hide");
+        $(".modal-body").text("Could not scrape at this time");
+        $("#articles-scraped-modal").modal("show");
+      }
     });
+  });
 
-    $(".clear-reviews").on("click", function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: "/scrape",
-            type: "DELETE"
-        }).then(function() {
-            location.reload();
-        })
+  $(".clear-reviews").on("click", function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/scrape",
+      type: "DELETE"
+    }).then(function () {
+      location.reload();
     })
+  })
 
-    $("#articles-scraped-modal").on("hidden.bs.modal", function() {
-        location.reload();
+  $("#articles-scraped-modal").on("hidden.bs.modal", function () {
+    location.reload();
+  });
+
+  $(".comments-modal").on("hidden.bs.modal", function () {
+    location.reload();
+  });
+
+  $(".save-article").on("click", function (event) {
+    event.preventDefault();
+    let articleId = $(this).attr("data-id")
+    $.ajax({
+      url: "article/" + articleId,
+      type: "PUT",
+      data: { saved: true }
+    }).then(function () {
+      location.reload();
     });
+  });
 
-    $(".comments-modal").on("hidden.bs.modal", function() {
-        location.reload();
+  $(".unsave-article").on("click", function (event) {
+    event.preventDefault();
+    let articleId = $(this).attr("data-id")
+    $.ajax({
+      url: "article/" + articleId,
+      type: "PUT",
+      data: { saved: false }
+    }).then(function () {
+      location.reload();
     });
+  });
 
-    $(".save-article").on("click", function(event) {
-        event.preventDefault();
-        let articleId = $(this).attr("data-id")
-        $.ajax({
-            url: "article/" + articleId,
-            type: "PUT",
-            data: {saved: true}
-        }).then(function() {
-            location.reload();
-        });
-    });
+  $(".add-comment").on("click", function (event) {
+    event.preventDefault();
+    let articleId = $(this).attr("data-id");
+    $("#comments-modal-" + articleId).modal("show");
+  })
 
-    $(".unsave-article").on("click", function(event) {
-        event.preventDefault();
-        let articleId = $(this).attr("data-id")
-        $.ajax({
-            url: "article/" + articleId,
-            type: "PUT",
-            data: {saved: false}
-        }).then(function() {
-            location.reload();
-        });
-    });
-
-    $(".add-comment").on("click", function(event) {
-        event.preventDefault();
-        let articleId = $(this).attr("data-id");
-        $("#comments-modal-" + articleId).modal("show");
+  $(".submit-comment").on("click", function (event) {
+    event.preventDefault();
+    let articleId = $(this).attr("data-id");
+    $.ajax({
+      url: "/article/" + articleId,
+      type: "POST",
+      data: {
+        name: $("#name-input-" + articleId).val(),
+        body: $("#comment-area-" + articleId).val()
+      }
+    }).then(function () {
+      $("#comments-modal-" + articleId).modal("hide");
     })
+  })
 
-    $(".submit-comment").on("click", function(event) {
-        event.preventDefault();
-        let articleId = $(this).attr("data-id");
-        $.ajax({
-            url: "/article/" + articleId,
-            type: "POST",
-            data: {
-                name: $("#name-input-" + articleId).val(),
-                body: $("#comment-area-" + articleId).val()
-            }
-        }).then(function() {
-            $("#comments-modal-" + articleId).modal("hide");
-        })
+  $(".delete-comment").on("click", function (event) {
+    event.preventDefault();
+    let commentId = $(this).attr("data-id");
+    let articleId = $(this).parent().attr("data-id");
+    $.ajax({
+      url: "/article/" + articleId + "/comment/" + commentId,
+      type: "DELETE"
+    }).then(function () {
+      $("#comments-modal-" + articleId).modal("hide");
     })
-
-    $(".delete-comment").on("click", function(event) {
-        event.preventDefault();
-        let commentId = $(this).attr("data-id");
-        let articleId = $(this).parent().attr("data-id");
-        $.ajax({
-            url: "/article/" + articleId + "/comment/" + commentId,
-            type: "DELETE"
-        }).then(function() {
-            $("#comments-modal-" + articleId).modal("hide");
-        })
-    })
+  })
 
 });
